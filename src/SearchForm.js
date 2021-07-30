35d5adec1a31a70eb0882947f8a0aee2c0a5dc20
Pabelara <80460781+Pabelara@users.noreply.github.com>
2021-07-30 14:58:36 +0100

    Added Working Search Form
    Moved weather information to WeatherInfo.js. Search form now works. Next Step: Fix weather icon and degrees.

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
+  let [city, setCity] = useState(props.defaultCity);
   let [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -19,82 +20,61 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    let apiKey = "935d274474278d462bed68ee689c049b";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
-      <form className="search-form">
-        <div className="row">
-          <div className="col-sm-8">
-            <input
-              type="search"
-              placeholder="Search City"
-              autoFocus="on"
-              autoComplete="off"
-              id="searchCity"
-              className="form-control shadow-sm"
-            />
+      <div className="hero">
+        <form className="search-form" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-sm-8">
+              <input
+                type="search"
+                placeholder="Search City"
+                className="form-control shadow-sm"
+                id="searchCity"
+                autoFocus="on"
+                autoComplete="off"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-sm-2">
+              <input
+                type="submit"
+                value="Search"
+                id="searchButton"
+                className="form-control shadow-sm"
+              />
+            </div>
+            <div className="col-sm-2">
+              <input
+                type="submit"
+                value="Current"
+                id="currentLocationButton"
+                className="form-control shadow-sm"
+              />
+            </div>
           </div>
-          <div className="col-sm-2">
-            <input
-              type="submit"
-              value="Search"
-              id="searchButton"
-              className="form-control shadow-sm"
-            />
-          </div>
-          <div className="col-sm-2">
-            <input
-              type="submit"
-              value="Current"
-              id="currentLocationButton"
-              className="form-control shadow-sm"
-            />
-          </div>
-        </div>
+        </form>
         <br />
-        <h1 id="city">{weatherData.city}</h1>
-        <FormattedDate date={weatherData.date} />
-
-        <div className="row">
-          <div className="offset-2 col-sm-7 d-flex justify-content-center">
-            <img
-              src="http://openweathermap.org/img/wn/01d@2x.png"
-              alt={weatherData.description}
-              className="icon"
-              id="icon"
-            />
-            <p className="description">{weatherData.description}</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-7 temperature">{weatherData.temperature}</div>
-          <div className=" col-5">
-            <span className="degree">ºC | ºF</span>
-            <ul className="attributes">
-              <li className="humidity" id="humidity">
-                Humidity: {weatherData.humidity}%
-              </li>
-              <li className="wind" id="wind">
-                Wind Speed: {weatherData.wind}km/h
-              </li>
-            </ul>
-          </div>
-        </div>
-        <br />
-        <div className="realFeeling">
-          <ul>
-            <li>Real Feeling</li>
-            <li className="realTemp" id="realTemp">
-              {weatherData.realFeeling}ºC
-            </li>
-          </ul>
-        </div>
-      </form>
+        <WeatherInfo data={weatherData} />
+      </div>
     );
   } else {
-    let apiKey = "935d274474278d462bed68ee689c049b";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,45 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="weatherInfo">
+      <h1 id="city">{props.data.city}</h1>
+      <FormattedDate date={props.data.date} />
+      <div className="row">
+        <div className="offset-2 col-sm-7 d-flex justify-content-center">
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            alt={props.data.description}
+            className="icon"
+            id="icon"
+          />
+          <p className="description">{props.data.description}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-7 temperature">{props.data.temperature}</div>
+        <div className=" col-5">
+          <span className="degree">ºC | ºF</span>
+          <ul className="attributes">
+            <li className="humidity" id="humidity">
+              Humidity: {props.data.humidity}%
+            </li>
+            <li className="wind" id="wind">
+              Wind Speed: {props.data.wind}km/h
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <div className="realFeeling">
+        <ul>
+          <li>Real Feeling</li>
+          <li className="realTemp" id="realTemp">
+            {props.data.realFeeling}ºC
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
